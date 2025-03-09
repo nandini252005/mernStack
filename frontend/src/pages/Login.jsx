e6d6b611dfabe962407bc0e44a09264e168721d2
sameer shaik <sameer5471112@gmail.com>
2025-03-09 16:58:05 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,40 +2,39 @@ import { useState, useContext } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import { BASE_URL } from "../config";
 import { toast } from "react-toastify";
-import { authContext } from '../context/AuthContext.jsx'
-import  HashLoader  from "react-spinners/HashLoader";
+import { authContext } from '../context/AuthContext.jsx';
+import HashLoader from "react-spinners/HashLoader";
 
 const Login = () => {
     const [formData, setFormData] = useState({
-        email:'',
-        password:''
+        email: '',
+        password: ''
     });
 
-    const [loading, setLoading] = useState(false)
-    const navigate = useNavigate()
-    const { dispatch } = useContext(authContext)
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const { dispatch } = useContext(authContext);
 
     const handleInputChange = e => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
     const submitHandler = async event => {
-
         event.preventDefault();
         setLoading(true);
 
-        try{
+        try {
             const res = await fetch(`${BASE_URL}/auth/login`, {
-                method:"post",
-                headers:{
-                    'Content-Type':'application/json'
+                method: "post",
+                headers: {
+                    'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(formData),
-            })
+            });
 
-            const result = await res.json()
+            const result = await res.json();
 
-            if(!res.ok){
+            if (!res.ok) {
                 throw new Error(result.message);
             }
 
@@ -56,9 +55,9 @@ const Login = () => {
         } catch (err) {
             toast.error(err.message);
             setLoading(false);
-
         }
     };
+
     return (
         <section className="px-5 lg:px-0">
             <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10">
@@ -67,46 +66,46 @@ const Login = () => {
                 </h3>
                 <form className="py-4 md:py-0" onSubmit={submitHandler}>
                     <div className="mb-5">
-                        <input 
-                        type="email" 
-                        placeholder="Enter Your Email" 
-                        name="email" 
-                        value={formData.email}  
-                        onChange={handleInputChange} 
-                        className="w-full py-3 border-b border-solid border-[#0066ff61] focus:outline-none 
-                        focus:border-b-primaryColor text-[16px] leading-7 text-headingColor 
-                        placeholder:text-textColor curcor-pointer" 
-                        required
+                        <input
+                            type="email"
+                            placeholder="Enter Your Email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleInputChange}
+                            className="w-full py-3 border-b border-solid border-[#0066ff61] focus:outline-none 
+                            focus:border-b-primaryColor text-[16px] leading-7 text-headingColor 
+                            placeholder:text-textColor cursor-pointer"
+                            required
                         />
                     </div>
                     <div className="mb-5">
-                        <input 
-                        type="password" 
-                        placeholder="Password"
-                         name="password" 
-                         value={formData.password}  
-                         onChange={handleInputChange} 
-                         className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none 
-                         focus:border-b-primaryColor text-[16px] leading-7 text-headingColor 
-                         placeholder:text-textColor rounded-md curcor-pointer" 
-                         required
-                         />
+                        <input
+                            type="password"
+                            placeholder="Enter Your Password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleInputChange}
+                            className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none 
+                            focus:border-b-primaryColor text-[16px] leading-7 text-headingColor 
+                            placeholder:text-textColor rounded-md cursor-pointer"
+                            required
+                        />
                     </div>
                     <div className="mt-7">
                         <button type="submit" className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3">
-                             { loading ? <HashLoader size={25} color="#fff" /> : ' Kiru'} 
+                            {loading ? <HashLoader size={25} color="#fff" /> : 'Login'}
                         </button>
                     </div>
                     <p className="mt-5 text-textColor text-center">
-                        Don't have an account? 
+                        Don't have an account?
                         <Link to="/register" className="text-primaryColor font-medium ml-1">
-                            Tirkelu
+                            Register
                         </Link>
                     </p>
                 </form>
             </div>
         </section>
-    )
+    );
 };
 
 export default Login;
